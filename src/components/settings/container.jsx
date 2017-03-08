3aa449c68d7308e3ad69813b3fcc6485293d2e61
Yashika Garg <yashikagarag13@gmail.com>
2017-03-08 16:48:27 +0530

    BM - Settings Page - Container view:
    Add event handler to check expense entries on click of delete
    Add method to show and hide ChooseCategory modal
    Add method to update bulk expense entries with new categories
    Add method to delete category

--- src/components/settings/container.jsx
+++ src/components/settings/container.jsx
@@ -6,40 +6,50 @@ import Helpers from "../../helpers/index";
 import Settings from "./view";
 
 class SettingsContainer extends React.Component {
+  // Lifecycle Methods
   constructor (props) {
     super(props);
 
     this.originalCategories = [];
     this.state = {
       categories: [],
-      newCategoryTitle: "",
+      chooseCategoryModalData: null,
+      confirmModalData: null,
       isConfirmModalVisible: false,
+      isChooseCategoryModalVisible: false,
+      newCategoryIdForExpenses: null,
+      newCategoryTitle: "",
     };
 
-    this.onChangeCategoryTitle = this.onChangeCategoryTitle.bind(this);
-    this.onChangeNewCategoryTitle = this.onChangeNewCategoryTitle.bind(this);
+    this.onChangeCategoryTitleInput = this.onChangeCategoryTitleInput.bind(this);
+    this.onChangeNewCategoryTitleInput = this.onChangeNewCategoryTitleInput.bind(this);
     this.onClickAdd = this.onClickAdd.bind(this);
+    this.onClickDelete = this.onClickDelete.bind(this);
+    this.onClickEdit = this.onClickEdit.bind(this);
+    this.onClickEditDone = this.onClickEditDone.bind(this);
     this.onEditCategory = this.onEditCategory.bind(this);
     this.onHideConfirmModal = this.onHideConfirmModal.bind(this);
-    this.onShowEditMode = this.onShowEditMode.bind(this);
-    this.onShowConfirmModal = this.onShowConfirmModal.bind(this);
   }
   componentWillMount () {
     Helpers.Utils.redirectToLoginIfTokenExpired(this.props.router);
-    this.loadData();
+    this.onLoadData();
   }
 
-  addEditModeToCategories (categories) {
-    return R.map(category => {
-      category.editMode = false;
-      return category;
-    }, categories);
+  // Event Listeners
+  hasExpenseEnteriesByCategory(categoryId) {
+    return Helpers.API.getExpenseEnteriesByCategoryAndUser(categoryId)
+      .then(response => {
+        Helpers.Utils.redirectToLoginIfTokenExpired(this.props.router);
+        if (response.success) {
+          return response.data.length;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        return 0;
+      });
   }
-  isCategoryTitleUpdated(category, originalCategories) {
-    const oldCategory = R.find(R.propEq("_id", category._id), originalCategories);
-    return oldCategory.title != category.title;
-  }
-  onChangeCategoryTitle (categoryId) {
+  onChangeCategoryTitleInput (categoryId) {
     let categories = this.state.categories;
     const index = R.findIndex(R.propEq("_id", categoryId), categories);
     categories[index].title = event.target.value;
@@ -48,9 +58,14 @@ class SettingsContainer extends React.Component {
       categories: categories,
     });
   }
-  onChangeNewCategoryTitle (event) {
+  onChangeNewCategoryTitleInput (event) {
     this.setState({
       newCategoryTitle: event.target.value,
+    });
+  }
+  onChangeNewCategoryInput (event) {
+    this.setState({
+      newCategoryIdForExpenses: event.target.value,
     });
   }
   onClickAdd () {
@@ -64,18 +79,50 @@ class SettingsContainer extends React.Component {
         if(response.success) {
           let categories = this.state.categories;
           categories = R.prepend(response.data, categories);
-          this.setState({categories});
+          this.setState({categories, newCategoryTitle: ""});
         }
       })
       .catch((error) => {
         console.log(error);
       });
   }
-  onHideConfirmModal() {
-    this.setState({
-      isConfirmModalVisible: false,
-      modalData: null
-    });
+  onClickDelete (category) {
+    const categoryId = category._id;
+    if(R.type(categoryId) == "String" && !R.isEmpty(categoryId)) {
+      this.hasExpenseEnteriesByCategory(categoryId)
+        .then((length) => {
+          if (length > 0) {
+            this.onShowChooseCategoryModal(categoryId);
+          } else {
+            this.onDeleteCategory(categoryId);
+          }
+        });
+    } else {
+      let categories = this.state.categories;
+      let index = R.findIndex(R.propEq("title", category.title), categories);
+      this.deleteCategoryFromCategories(index, categories);
+    }
+    // call api to get expense entries of given category of logged in user
+    // if expense entries exists
+    //    show modal to choose category for all those expenses
+    //      - bulk update expenses
+    //        - delete category
+    // else
+    //    delete category
+  }
+  onClickEdit (categoryId) {
+    let categories = this.state.categories;
+    this.setCategoryEditMode(categoryId, categories, true);
+  }
+  onClickEditDone(category) {
+    if (this.isCategoryTitleUpdated(category, this.originalCategories)) {
+      this.setState({
+        isConfirmModalVisible: true,
+        confirmModalData: category,
+      });
+    } else {
+      this.setCategoryEditMode(category._id, this.state.categories, false);
+    }
   }
   onEditCategory (category) {
     let categories = this.state.categories;
@@ -93,22 +140,31 @@ class SettingsContainer extends React.Component {
         this.onHideConfirmModal();
       });
   }
-  onShowConfirmModal(category) {
-    if (this.isCategoryTitleUpdated(category, this.originalCategories)) {
-      this.setState({
-        isConfirmModalVisible: true,
-        modalData: category,
-      });
-    } else {
-      this.setCategoryEditMode(category._id, this.state.categories, false);
-    }
-  }
-  onShowEditMode (categoryId) {
+  onDeleteCategory (categoryId) {
     let categories = this.state.categories;
-    this.setCategoryEditMode(categoryId, categories, true);
+    Helpers.API.deleteCategory(categoryId)
+      .then(response => {
+        Helpers.Utils.redirectToLoginIfTokenExpired(this.props.router);
+        if(response.success) {
+          const index = R.findIndex(R.propEq("_id", categoryId), categories);
+          this.deleteCategoryFromCategories(index, categories);
+          if (this.state.isChooseCategoryModalVisible) {
+            this.onHideChooseCategoryModal();
+          }
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-  loadData () {
-    Helpers.API.getExpensCategoriesByUser()
+  onHideConfirmModal() {
+    this.setState({
+      isConfirmModalVisible: false,
+      confirmModalData: null
+    });
+  }
+  onLoadData () {
+    Helpers.API.getExpenseCategoriesByUser()
       .then(response => {
         Helpers.Utils.redirectToLoginIfTokenExpired(this.props.router);
         if (response.success) {
@@ -122,6 +178,47 @@ class SettingsContainer extends React.Component {
         console.log(error);
       });
   }
+  onShowChooseCategoryModal (categoryId) {
+    this.setState({
+      isChooseCategoryModalVisible: true,
+      chooseCategoryModalData: categoryId,
+    });
+  }
+  onHideChooseCategoryModal () {
+    this.setState({
+      isChooseCategoryModalVisible: false,
+      chooseCategoryModalData: null,
+    });
+  }
+  onUpdateExpensesWithCategory (oldCategoryId) {
+    const newCategoryId = this.state.newCategoryIdForExpenses;
+    Helpers.API.updateCategoryInExpenseEntries(oldCategoryId, newCategoryId)
+      .then(response => {
+        Helpers.Utils.redirectToLoginIfTokenExpired(this.props.router);
+        if(response.success) {
+          this.onDeleteCategory(oldCategoryId);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  // Helper Methods
+  addEditModeToCategories (categories) {
+    return R.map(category => {
+      category.editMode = false;
+      return category;
+    }, categories);
+  }
+  deleteCategoryFromCategories (index, categories) {
+    categories = R.remove(index, 1, categories);
+    this.setState({categories});
+  }
+  isCategoryTitleUpdated(category, originalCategories) {
+    const oldCategory = R.find(R.propEq("_id", category._id), originalCategories);
+    return oldCategory.title != category.title;
+  }
   setCategoryEditMode(categoryId, categories, isEditMode) {
     const index = R.findIndex(R.propEq("_id", categoryId), categories);
     categories[index].editMode = isEditMode;
@@ -132,18 +229,26 @@ class SettingsContainer extends React.Component {
   render () {
     return (
       <Settings
-        categories={this.state.categories}
-        updateNewCategoryTitle={this.onChangeNewCategoryTitle}
         addNewCategory={this.onClickAdd}
+        categories={this.state.categories}
+        newCategoryTitle={this.state.newCategoryTitle}
+        updateNewCategoryTitleInput={this.onChangeNewCategoryTitleInput}
 
         confirmMsgText={Helpers.Notifictaions.confirmEditCategory}
         editCategory={this.onEditCategory}
         hideConfirmModal={this.onHideConfirmModal}
         isConfirmModalVisible={this.state.isConfirmModalVisible}
-        modalData={this.state.modalData}
-        showConfirmModal={this.onShowConfirmModal}
-        showEditMode={this.onShowEditMode}
-        updateCategoryTitle={this.onChangeCategoryTitle}
+        confirmModalData={this.state.confirmModalData}
+        showConfirmModal={this.onClickEditDone}
+        showEditMode={this.onClickEdit}
+        updateCategoryTitleInput={this.onChangeCategoryTitleInput}
+        
+        checkExpenses={this.onClickDelete}
+        isChooseCategoryModalVisible={this.state.chooseCategoryModalData}
+        chooseCategoryModalData={this.state.chooseCategoryModalData}
+        hideChooseCategoryModal={this.onHideChooseCategoryModal}
+        updateNewCategoryInput={this.onChangeNewCategoryInput}
+        updateExpensesWithCategory={this.onUpdateExpensesWithCategory}
       />
     );
   }
