ef133bc95c1bdc1eec281f0229556675b28956ff
Yashika Garg <yashikagarag13@gmail.com>
2017-03-21 17:17:29 +0530

    BM - Refactor expense list in separate component

--- src/components/landing/container.jsx
+++ src/components/landing/container.jsx
@@ -8,104 +8,16 @@ import Landing from "./view";
 class LandingContainer extends Component {
   constructor (props) {
     super(props);
-    this.total = 0;
-    this.state = {
-      entries: [],
-      loading: false,
-      page: 0,
-    };
-
-    this.onClickExpenseEdit = this.onClickExpenseEdit.bind(this);
-    this.onClickExpenseRemove = this.onClickExpenseRemove.bind(this);
-    this.onEnterWaypoint = this.onEnterWaypoint.bind(this);
   }
-  componentWillMount () {
-    Helpers.Utils.redirectToLoginIfTokenExpired(this.props.router);
-    this.loadData(this.state.page);
-  }
-
-  getOffset(page, perPage, total) {
-    let offset = page * perPage;
-
-    if (page < 0) {
-      return 0;
-    }
-
-    if (offset >= total)  {
-      return this.total;
-    }
-
-    if (page >= 0 && offset < this.total) {
-      return offset;
-    }
-  }
-  loadData (page) {
-    Helpers.API.getExpenseEnteriesByUser(Helpers.Constants.perPage, page)
-    .then(response => {
-      Helpers.Utils.redirectToLoginIfTokenExpired(this.props.router);
-      if (response.success) {
-        this.total = response.total;
-        this.setState({
-          entries: R.concat(this.state.entries, response.data),
-          loading: false,
-        });
-      }
-    })
-    .catch((error) => {
-      console.log(error); // eslint-disable-line
-      this.setState({loading: false});
-    });
-  }
-
-
-  onEnterWaypoint() {
-    if (this.state.loading) return;
-    this.setState({loading: true});
-
-    let page = this.state.page;
-
-    if (this.getOffset(page, Helpers.Constants.perPage, this.total) < this.total) {
-      this.setState({page: page + 1});
-      this.loadData(page + 1);
-    } else {
-      this.setState({loading: false});
-    }
-  }
-  onClickExpenseEdit (expenseId) {
-    this.props.router.push(`edit-entry/${expenseId}`);
-  }
-  onClickExpenseRemove (expenseId) {
-    Helpers.API.removeExpenseEntry(expenseId)
-      .then(response => {
-        Helpers.Utils.redirectToLoginIfTokenExpired(this.props.router);
-        if (response.success) {
-          const index = R.findIndex(expense => expense._id == expenseId, this.state.entries);
-          let entries = this.state.entries;
-          entries = R.remove(index, 1, entries);
-          this.setState({entries});
-        }
-      })
-      .catch((error) => {
-        console.log(error); // eslint-disable-line
-      });
-  }
-
   render () {
     return (
-      <Landing
-        entries={this.state.entries}
-        isLoading={this.state.loading}
-        showWaypoint={this.total > this.getOffset(this.state.page, Helpers.Constants.perPage, this.total)}
-        enterWaypoint={this.onEnterWaypoint}
-        updateExpenseEntry={this.onClickExpenseEdit}
-        removeExpenseEntry={this.onClickExpenseRemove}
-      />
+      <Landing  />
     );
   }
 }
 
 LandingContainer.propTypes = {
-  router: PropTypes.object,
+  location: PropTypes.object,
 };
 
 export default LandingContainer;
--- src/components/landing/view.jsx
+++ src/components/landing/view.jsx
@@ -1,67 +1,18 @@
 import R from "ramda";
 import React, {PropTypes} from "react";
-import Waypoint from "react-waypoint";
 
 import Header from "../common/header";
-import Loading from "../common/loading";
+import ExpensesContainer from "../expenses/container";
 
 const Landing = (props) => (
   <div className="landing-view">
     <Header actions={["Settings", "Add"]}/>
-    <div className="container">
-      <div className="list-group padding-top padding-bottom">
-        {R.map(expense =>
-          <div className="list-group-item" key={`expense${expense._id}`}>
-            <div className="row">
-              <div className="col-sm-8">
-                <span className="xlg bold text">{expense.category ? expense.category.title : ""}</span><br/>
-                <span className="sm light-imp text">{expense.description}</span>
-              </div>
-              <div className="col-sm-4 text-right">
-                <div className="accent xlg text">
-                  {expense.currency ? <i className={`fa fa-${R.toLower(expense.currency)}`}></i> : null}&nbsp;
-                  <span className="xxlg text">{expense.amount}</span>
-                </div>
-                <div>
-                  <button className="btn-icon"
-                    onClick={props.updateExpenseEntry.bind(null, expense._id)}>
-                    <i className="fa fa-pencil" aria-hidden="true"></i>
-                  </button>
-                  <button className="btn-icon"
-                    onClick={props.removeExpenseEntry.bind(null, expense._id)}>
-                    <i className="fa fa-minus-circle" aria-hidden="true"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>,
-        props.entries)}
-      </div>
-      {props.showWaypoint
-        ? <Waypoint onEnter={props.enterWaypoint}>
-            <div>
-              {props.isLoading ? <Loading attachToParent/> : null}
-            </div>
-          < /Waypoint>
-        : null}
-    </div>
+    <ExpensesContainer />
   </div>
 );
 
-function hasCategory(expense) {
-  return !R.isEmpty(expense.category) && R.type(expense.category) == "Object";
-}
 
 Landing.propTypes = {
-  entries: PropTypes.array.isRequired,
-
-  isLoading: PropTypes.bool,
-  showWaypoint: PropTypes.bool,
-  enterWaypoint: PropTypes.func.isRequired,
-
-  removeExpenseEntry: PropTypes.func.isRequired,
-  updateExpenseEntry: PropTypes.func.isRequired,
 };
 
 export default Landing;
-// <pre><code>{JSON.stringify(props.entries, null, 4)}</code></pre>
